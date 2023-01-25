0d89c6efd80e23be3be8e2ad0c13bc2571e2a045
ShafayatBayezid <shafayat.h.b@gmail.com>
2023-01-25 18:57:35 +0600

    Assignment module - 01

--- module01.php
+++ module01.php
@@ -1,20 +1,5 @@
 <?php
-$num = -110;
-// Using if statement
-if ($num > 0) {
-  echo $result = 'positive';
-}
-elseif ($num < 0 ){
-  echo $result = 'negative';
-  if( $num < -10) {
-    echo $result = 'value is below -10';
-  }
-}
-else {
-  echo $result = 'its zero';
-}
-
-echo "\n";
+$num = 5;
 
 $result = (0 < $num) ? "Positive" : ( (0 > $num) ? ( (-10 > $num) ? "value is below -10" : "Negative" ) : "its zero" );
 
