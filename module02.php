f9ba7b7373339e9e149a682b9745975384aa0b3c
ShafayatBayezid <shafayat.h.b@gmail.com>
2023-02-06 00:44:03 +0600

    Assignment Module 02

--- module02.php
+++ module02.php
@@ -1,9 +1,12 @@
 <?php
+$tuition_fee = 12000;
 
-$n = 12;
+$commission = ($tuition_fee > 20000) ? 0.25 * $tuition_fee :
+(($tuition_fee > 10000) ? 0.20 * $tuition_fee :
+    (($tuition_fee > 7000) ? 0.15 * $tuition_fee :
+        "Invalid data"));
 
-if ( $n % 2 == 0 ) {
-    echo "$n is an even number";
-} else {
-    echo "$n is an odd number";
-}
+echo "25% Commission on above 20000 dollars \n";
+echo "20% Commission on above 10000 dollars \n";
+echo "15% Commission on above 7000 dollars \n \n";
+echo "Your Commission of {$tuition_fee} dollars is {$commission} dollars \n";
