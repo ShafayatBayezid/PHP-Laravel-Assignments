3a785b56930c6a5084cf49025c538f536808fac8
ShafayatBayezid <shafayat.h.b@gmail.com>
2023-02-06 12:30:41 +0600

    Assignment Module 02

--- module02.php
+++ module02.php
@@ -1,12 +1,10 @@
 <?php
-$tuition_fee = 122000;
+$tuition_fee = 25000;
 
-$commission = ($tuition_fee > 20000) ? " 25% = " . 0.25 * $tuition_fee :
-(($tuition_fee > 10000) ? " 20% = " . 0.20 * $tuition_fee :
-    (($tuition_fee > 7000) ? " 15% = " . 0.15 * $tuition_fee :
+$commission = ($tuition_fee > 20000) ? "Commission of {$tuition_fee} dollars is 25% = " . 0.25 * $tuition_fee . " dollars" :
+(($tuition_fee > 10000) ? "Commission of {$tuition_fee} dollars is 20% = " . 0.20 * $tuition_fee . " dollars" :
+    (($tuition_fee > 7000) ? "Commission of {$tuition_fee} dollars is 15% = " . 0.15 * $tuition_fee . " dollars" :
         "Invalid data"));
 
-echo "25% Commission on above 20000 dollars \n";
-echo "20% Commission on above 10000 dollars \n";
-echo "15% Commission on above 7000 dollars \n \n";
-echo "Commission of {$tuition_fee} dollars is {$commission} dollars \n";
+echo "\n";
+echo " {$commission} \n";
